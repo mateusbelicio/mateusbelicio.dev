4665fbe0f8352293dc1ec50699ab3d0c631b74f4
Mateus Belicio <mateusbelicio@hotmail.com>
2024-01-18 21:06:45 -0300

    feat: reveal animation component

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
-
+import RevealAnimation from '../features/reveal-animation';
 import { Button, ButtonIcon } from '../ui/button';
 
 function HeroSection() {
   return (
     <section className="pb-12 lg:pb-18">
-      <div className="main-container main-grid relative gap-y-6">
+      <RevealAnimation className="main-container main-grid relative gap-y-6">
+        <div className="relative lg:col-start-7 lg:row-start-1"></div>
         <div className="z-10 flex flex-col items-start gap-8 pt-14 sm:col-start-1 sm:col-end-10 sm:gap-12 sm:self-end sm:pr-14 sm:pt-14 md:row-start-1 lg:col-end-8 lg:gap-[3.25rem]">
           <h1 className="heading-1 min-w-[19.25rem] max-w-sm">
             Hey, I’m Mateus Belício and I love building beautiful websites
@@ -19,7 +19,7 @@ function HeroSection() {
             </Button>
           </div>
         </div>
-      </div>
+      </RevealAnimation>
     </section>
   );
 }
