550c7578ea6d221d7ee7b63d26ea536e7ab7e594
Mateus Belicio <mateusbelicio@hotmail.com>
2024-01-31 07:54:52 -0300

    fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,8 +4,7 @@ import { allProjects } from '@/content/portfolio';
 const projectsMap: MetadataRoute.Sitemap = allProjects.map((project) => ({
   url: `${process.env.NEXT_PUBLIC_APP_URL}/portfolio/${project.slug}`,
   lastModified: new Date(),
-  changeFrequency: 'yearly',
-  priority: 0.5,
+  priority: 0.64,
 }));
 
 export default function sitemap(): MetadataRoute.Sitemap {
@@ -13,20 +12,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${process.env.NEXT_PUBLIC_APP_URL}/`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
+      priority: 1.0,
     },
     {
       url: `${process.env.NEXT_PUBLIC_APP_URL}/portfolio`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
       url: `${process.env.NEXT_PUBLIC_APP_URL}/contact`,
       lastModified: new Date(),
-      changeFrequency: 'never',
-      priority: 0.5,
+      priority: 0.8,
     },
     ...projectsMap,
   ];
